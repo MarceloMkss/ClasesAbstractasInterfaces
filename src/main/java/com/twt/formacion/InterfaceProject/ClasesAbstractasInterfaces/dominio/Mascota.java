2b21445f838ad08157b5f4990ef765b0e96a4b6b
MarceloMkss <marcelomarcelomkss@gmail.com>
2021-08-25 21:13:44 +0200

    Varias clases de animales implementan una interfaz llamada Mascota

---
+++ src/main/java/com/twt/formacion/InterfaceProject/ClasesAbstractasInterfaces/dominio/Mascota.java
@@ -0,0 +1,16 @@
+package com.twt.formacion.InterfaceProject.ClasesAbstractasInterfaces.dominio;
+
+public interface Mascota {
+	
+	public interface Pet {
+		
+		
+		public String getName();
+		public void setName(String n);
+		public void play();
+		
+	}
+	
+	
+
+}
