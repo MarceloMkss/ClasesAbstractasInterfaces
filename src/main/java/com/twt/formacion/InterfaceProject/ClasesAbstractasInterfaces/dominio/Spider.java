c6107c60ea2eb5d680960f2f1fa57b5a161b17f5
MarceloMkss <marcelomarcelomkss@gmail.com>
2021-08-25 21:16:54 +0200

    Clase Spider que extends de la clase  Animal.

---
+++ src/main/java/com/twt/formacion/InterfaceProject/ClasesAbstractasInterfaces/dominio/Spider.java
@@ -0,0 +1,24 @@
+package com.twt.formacion.InterfaceProject.ClasesAbstractasInterfaces.dominio;
+
+
+/**
+ * 
+ * @author Marcelo Kleibson
+ * @version 1.0
+ */
+public class Spider extends Animal{
+	
+	
+	public Spider() {
+		super(8);
+		}
+		
+	
+	
+	public void eat() {
+		
+		// The spider eats a fly.
+		System.out.println("La araña se come una mosca.");
+	}
+
+}
